4efe46f4254399d8100d97f8d65f992f5ea5bed7
Luke Donnet <Luke Donnet>
2017-08-19 21:37:33 +1000

    setup development environment

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-    entry: './src/test.js',
+    entry: './src/app.js',
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'app.bundle.js'
