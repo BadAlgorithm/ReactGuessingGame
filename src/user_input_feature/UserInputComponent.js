afdff90b43ab2655f6912fac5226a28ab777ee0b
Luke Donnet <luke.donnet@my.jcu.edu.au>
2017-11-01 22:53:28 +1000

    implemented continuous deployment

--- src/user_input_feature/UserInputComponent.js
+++ src/user_input_feature/UserInputComponent.js
@@ -4,7 +4,7 @@ export default () => {
     return (
         <div itemID="myid">
             <h1>User Input</h1>
-            <p>My User Input Feature</p>
+            <p>My User Input Feature, automatically deployed!</p>
         </div>
     )
 };
