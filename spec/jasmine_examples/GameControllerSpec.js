8eb58c166b55e11cfd44bc2c2042ec7514e4bd0e
Luke Donnet <luke.donnet@my.jcu.edu.au>
2018-01-14 13:04:56 +1000

    added scss colour palette

--- spec/jasmine_examples/GameControllerSpec.js
+++ spec/jasmine_examples/GameControllerSpec.js
@@ -1,7 +1,16 @@
 import GameController from "../../src/controller/controller"
 
 describe("GameController", function () {
-    it("Should correct if guess and target are equal", function () {
-        expect(GameController.getPrompt(1, 1)).toEqual("correct!");
-    });
+    it("Should correct if guess and target are equal",
+        () => expect(GameController.getPrompt(1, 1)).toEqual("correct!")
+    );
+    it("Should return correct number of guesses",
+        () => expect(GameController.countGuesses([1, 2, 3, 4])).toEqual(3)
+    );
+    it("Should return true when game is over",
+        () => expect(GameController.isGameOver(2, 2)).toEqual(true)
+    );
+    it("Should return false on an incorrect Guess",
+        () => expect(GameController.isGameOver(1,2)).toEqual(false)
+    );
 });
--- src/user_input_feature/UserInputComponent.js
+++ src/user_input_feature/UserInputComponent.js
@@ -1,10 +1,44 @@
 import React from "react";
+import {Input} from "semantic-ui-react";
 
-export default () => {
-    return (
-        <div itemID="myid">
-            <h1>User Input</h1>
-            <p>My User Input Feature, automatically deployed!</p>
-        </div>
-    )
-};
+class InputComponent extends React.Component {
+
+    constructor(){
+        super();
+        this.getInput = this.getInput.bind(this);
+        this.state = {numberOne: 0, numberTwo: 0}
+    }
+
+    getInput(event){
+        const id = event.target.id;
+        const value = parseInt(event.target.value)
+        const update = {};
+        switch (id){
+            case "inputOne":
+                update.numberOne = value;
+                break;
+            case "inputTwo":
+                update.numberTwo = value;
+                break;
+            default :
+                break;
+        }
+        this.setState(update, () => console.log(this.state));
+    };
+
+    render(){
+        const addNumbers = this.state.numberOne + this.state.numberTwo;
+        return (
+            <div itemID="myid">
+                <h1>User Input</h1>
+                <p>My User Input Feature, automatically deployed!</p>
+                <Input id="inputOne" onChange={this.getInput}/>
+                <Input id="inputTwo" onChange={this.getInput}/>
+                <p>{this.state.numberOne + this.state.numberTwo}</p>
+            </div>
+        )
+    }
+}
+
+
+export default InputComponent;
