3d05b6a60e5b3ba46d37daaceb22c000355a5524
Luke Donnet <lukedonnet@Lukes-MacBook-Pro.local>
2018-02-08 21:54:13 +1000

    changed to lcov reporting

--- karma.config.js
+++ karma.config.js
@@ -49,7 +49,7 @@ module.exports = function (config) {
             noInfo: true
         },
         coverageReporter: {
-            type: 'text-summary',
+            type: 'lcov',
             dir: 'coverage/'
         }
     });
