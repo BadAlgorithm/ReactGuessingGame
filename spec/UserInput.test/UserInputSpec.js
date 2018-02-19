8e951b70af0213dcea7d921e9d2257e8d72dfe9f
Luke Donnet <lukedonnet@Lukes-MacBook-Pro.local>
2018-02-19 18:19:34 +1000

    modified npm script name

--- spec/UserInput.test/UserInputSpec.js
+++ spec/UserInput.test/UserInputSpec.js
@@ -3,6 +3,5 @@ import {mapStateToProps, mapToDispatch} from "../../src/game_feature/user_input_
 describe("User Input Component", () => {
     it("should recieve an event with a 'userInput' key", () => {
         console.log(`props are ${mapStateToProps({userInput: 120})}`);
-        expect(mapToDispatch())
     })
 });
